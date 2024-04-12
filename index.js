4d3c86e01ccb5c75233ca86ec3b19149219caec5
Haleigh <haleighvalentine@gmail.com>
2024-04-12 02:27:58 +0200

    Fix indent

--- index.js
+++ index.js
@@ -26,9 +26,9 @@ fetch('https://api.coingecko.com/api/v3/coins/dogecoin')
             <span>${data.name}</span>
         `;
         document.getElementById('crypto').innerHTML += `
-        <p>🎯: R${data.market_data.current_price.zar}</p>
-        <p>👆: R${data.market_data.high_24h.zar}</p>
-        <p>👇: R${data.market_data.low_24h.zar}</p>
+            <p>🎯: R${data.market_data.current_price.zar}</p>
+            <p>👆: R${data.market_data.high_24h.zar}</p>
+            <p>👇: R${data.market_data.low_24h.zar}</p>
         `;
     })
     .catch((err) => console.error(err));
