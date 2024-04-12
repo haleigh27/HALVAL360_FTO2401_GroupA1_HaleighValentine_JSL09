e58688b54a491e1c6d38b72392aa74d26e1deca7
Haleigh <haleighvalentine@gmail.com>
2024-04-12 02:13:50 +0200

    Add crypto icon and name

--- index.js
+++ index.js
@@ -11,3 +11,19 @@ fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1502790671504-542ad42d5189?crop=entropy&cs=srgb&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTI4Nzg2NDF8&ixlib=rb-4.0.3&q=85)`;
         document.getElementById('author').textContent = `By:Mohammad Alizade`;
     });
+
+//Pull down the cryptocurrency data for dogecoin (image and name) from the CoinGecko API
+fetch('https://api.coingecko.com/api/v3/coins/dogecoin')
+    .then((res) => {
+        if (!res.ok) {
+            throw Error('Something went wrong');
+        }
+        return res.json();
+    })
+    .then((data) => {
+        document.getElementById('crypto-top').innerHTML = `
+            <img src=${data.image.small} />
+            <span>${data.name}</span>
+        `;
+    })
+    .catch((err) => console.error(err));
